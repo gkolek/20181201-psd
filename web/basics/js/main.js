c0f667004a7cd810e3b912c611da8eba2a5862eb
Arek Wtykło <arek.wtyklo@arteneoit.pl>
2018-11-25 10:49:29 +0100

    Basics, array content

--- web/basics/js/main.js
+++ web/basics/js/main.js
@@ -10,10 +10,36 @@ function updateArrayContentDOM() {
     // Iterujemy po kazdym elemencie w tablicy
     arr.forEach(function (value) {
         // Przygotowanie HTML z wartością
-        var contentDOM = '<div class="col-3"><div class="box">' + value + '</div></div>';
+        var contentDOM = '<div class="col-3"><div class="box"><div class="box-actions"><button class="btn btn-danger btn-sm" onclick="removeElement()"><i class="far fa-trash-alt"></i></button></div>' + value + '</div></div>';
         // Wstawienie do DOM przygotowanego HTML'a (przed końcem)
         arrayContentDOM.insertAdjacentHTML('beforeend', contentDOM);
     });
+}
+
+function pushElement() {
+    // 1. Musze wiedziec co dodaje = chce sciagnac z inputa tresc (value)
+    var inputDOM = document.getElementById('element-name');
+    // 2. Pobrac wartosc z inputa
+    var value = inputDOM.value;
+    if (value) {
+        // 3. Dodac element do tablicy o wartosci
+        arr.push(value);
+        // 4. Zaktualizować widok
+        updateArrayContentDOM();
+    }
+}
+
+function unshiftElement() {
+    // 1. Musze wiedziec co dodaje = chce sciagnac z inputa tresc (value)
+    var inputDOM = document.getElementById('element-name');
+    // 2. Pobrac wartosc z inputa
+    var value = inputDOM.value;
+    if (value) {
+        // 3. Dodac element do tablicy o wartosci
+        arr.unshift(value);
+        // 4. Zaktualizować widok
+        updateArrayContentDOM();
+    }
 }
 
 function removeFirst() {
